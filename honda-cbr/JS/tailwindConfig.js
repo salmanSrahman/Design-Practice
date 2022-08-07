8ac4775104ff96035be75b1a74c8e4dcf990fb62
Salman-S-Rahman <salmanmern@gmail.com>
2022-08-07 16:29:00 +0600

    nav part end

---
+++ honda-cbr/JS/tailwindConfig.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        primary: "#264653",
+        secondary: "#E76F51",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
