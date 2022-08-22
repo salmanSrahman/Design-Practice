093c3d681a204266dd1b8c5eb43c0b25e04a7486
Salman-S-Rahman <salmanmern@gmail.com>
2022-08-22 22:51:44 +0600

    all set up done

---
+++ Honda-cbr-tailwind/Js/tailwindConfig.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#264653",
+        secondary: "#E76F51",
+        third: "#23A6F0",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
