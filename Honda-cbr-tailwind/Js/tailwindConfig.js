4d61fc5bf23e85d151b4120ea6087cf28529f41a
Salman-S-Rahman <salmanmern@gmail.com>
2022-08-23 13:29:50 +0600

    banner part end

--- Honda-cbr-tailwind/Js/tailwindConfig.js
+++ Honda-cbr-tailwind/Js/tailwindConfig.js
@@ -5,6 +5,7 @@ tailwind.config = {
         primary: "#264653",
         secondary: "#E76F51",
         third: "#23A6F0",
+        fourth: "#E0E0E0",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
